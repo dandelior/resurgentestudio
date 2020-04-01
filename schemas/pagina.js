8da4bd6d0d11f5bec270d042461530bdfc086c2a
dandelior <jberrioscifuentes@gmail.com>
2020-03-31 22:22:23 -0300

    Slug in pagina

--- schemas/pagina.js
+++ schemas/pagina.js
@@ -9,6 +9,19 @@ export default {
             type: "string"
         },
         {
+            title: "Slug",
+            name: "slug",
+            type: "slug",
+            options: {
+                source: "titulo",
+                maxLength: 200,
+                slugify: input => input
+                         .toLowerCase()
+                         .replace(/\s+/g, '-')
+                         .slice(0, 200)
+            }
+        },
+        {
             title: "Contenido",
             name: "contenido",
             type: "array",
